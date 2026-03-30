08e8c5b22f1f7a5545638fe5090b916f4ae9855f
Manojkumar S <manojkumar.smk09@gmail.com>
2026-03-30 04:27:01 +0000

    pandas

--- 01python_basics/8-working_pandas.py
+++ 01python_basics/8-working_pandas.py
@@ -2,5 +2,27 @@ import pandas as pd
 
 df = pd.read_csv('dataset/nyc_weather.csv')
 
-print(df.head())
+# print(df.head())
+
+# print(df['EST'])
+
+# print(df['EST'][0])
+
+# print(df[['EST','DewPoint','Humidity']])
+
+# print(df['Temperature'].max())
+
+# print(df['EST'][df['Events'] == 'Rain'] )
+
+# df['Manoj'] = df['Temperature'] + 10
+# print(df[['Manoj', 'Temperature']])
+
+df['WindSpeedMPH'] = df['WindSpeedMPH'].fillna(0)
+
+print(df['WindSpeedMPH'].mean())
+
+
+
+
+
 
